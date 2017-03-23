2f0bde08ce7fd474de3fa4faaa683af8f9c9deed
[rchowdhury2] <[rchowdhury2@albany.edu]>
2017-03-22 21:50:59 -0400

    Node class created

---
+++ HW3.2/src/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+	public int data = 0;
+	public String name = "";
+	public Node next;
+	
+	public Node(int data, String name, Node next){
+		this.data = data;
+		this.name = name;
+		this.next = next;
+	}
+	
+	public String toString(){
+		return data + "";
+	}
+}
